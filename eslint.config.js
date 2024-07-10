c049eecafa2290e8ef9f1b6c6991a678a848dbbf
Matthieu <matthieu.delarue5@gmail.com>
2024-07-10 09:14:43 +0200

    🎨 Format code

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,7 @@
 const eslint = require("@eslint/js");
 const tseslint = require("typescript-eslint");
 const angular = require("angular-eslint");
-const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
-
+const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
 
 module.exports = tseslint.config(
   {
