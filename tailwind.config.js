4fc0a1f27c1203ad41040f3442dd016e4f82b295
Matthieu <matthieu.delarue5@gmail.com>
2024-10-24 21:13:51 -0700

    ➕Add tailwind forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
+  plugins: [require("@tailwindcss/forms")],
   theme: {
     extend: {
       fontFamily: {
@@ -91,5 +92,4 @@ module.exports = {
       },
     },
   },
-  plugins: [],
 };
